c09475eeed2a57acf1a3df29d8566d66017496de
Suman Ghosh <100sumanghosh@gmail.com>
2025-07-06 19:54:33 +0000

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "C++ Unit Tests with AI",
+  icons: "https://img.freepik.com/premium-vector/free-vector-beautiful-flying-hummingbird-design-element-banners-posters-leaflets-brochur_1009653-1.jpg",
   description: "Generate, refine, and optimize C++ unit tests with the power of AI.",
 };
 
